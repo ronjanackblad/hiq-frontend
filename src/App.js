afec06bad87714945382ff177418ded03b594395
ronjanackblad <ronja.nackblad@hotmail.com>
2020-04-01 13:37:20 +0200

    styling redone with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 
-import React, { Component } from 'react';     
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';   
 import FileForm from './components/FileForm';
 
-
-class App extends Component {    
-
-  render () {
-    return (
-          <div class="container">
-              <div class="header">
-                <h1>Word Counter</h1>
-                <p>~ Which is your most used word?</p>
-              </div>
-              <div class="form-container">
-                <p>This website will count the most used word in your text and add 'foo' + word + 'bar'</p>
-                <FileForm></FileForm>
-                <div class="info-box-container">
-                  <div class="info-box">
-                    <p>Did you know? <br/>Most common word in English is the word <a href="https://en.wikipedia.org/wiki/Most_common_words_in_English" target="_blank" title="https://en.wikipedia.org/wiki/Most_common_words_in_English">'the'</a></p>
-                  </div>
-                </div>
-              </div>
-              <div class="footer">
-                <p>Ronja Näckblad</p>
-              </div>
+const App = () => {
+  return (
+    <div class="container py-3">
+      <div className="d-flex flex-column justify-content-center align-items-center mt-5 p-2 shadow-lg p-3 mb-5 bg-white rounded">
+        <div class="header">
+          <h1>Word Counter</h1>
+          <p>~ Which is your most used word?</p>
+        </div>
+        <div>
+          <p>This website will count the most used word in your text and add 'foo' + word + 'bar'</p>
+          <FileForm></FileForm>
+          <div>
+            <div className="info-box rounded p-1 mt-2">
+              <p>Did you know? <br />Most common word in English is the word <a href="https://en.wikipedia.org/wiki/Most_common_words_in_English" target="_blank" title="https://en.wikipedia.org/wiki/Most_common_words_in_English">'the'</a></p>
+            </div>
           </div>
- 
-    );
-  }  
-}    
+        </div>
+      </div>
+    </div>
+
+  );
+
+}
 
 export default App;  
--- src/components/FileForm.js
+++ src/components/FileForm.js
@@ -1,56 +1,69 @@
 import React, { useState } from "react";
 import Axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /**
  * Component for handling the API request via a form, and saves the response in state
  */
 function FileForm() {
-    const [file, setFile] = useState(null);
-    const [text, setText] = useState("");
-    const [mostUsedWord, setMostUsedWord] = useState("");
-    const [freq, setFreq] = useState(null);
+  const [file, setFile] = useState(null);
+  const [text, setText] = useState("");
+  const [mostUsedWord, setMostUsedWord] = useState("");
+  const [freq, setFreq] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const fileSelectedHandler = event => {
-        setFile(event.target.files[0]);
-    }
+  const fileSelectedHandler = event => {
+    setFile(event.target.files[0]);
+  }
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        const fd = new FormData();
-        fd.append('file', file, file.name);
-        Axios.post('https://localhost:5001/Home/SingleFile', fd)
-          .then(res => {
-            console.log(res);
-            setText(res.data.text);
-            setMostUsedWord(res.data.mostUsedWord);
-            setFreq(res.data.frequencies);
-          });
-    };
+  const handleSubmit = e => {
+    e.preventDefault();
+    setLoading(true)
+    const fd = new FormData();
+    fd.append('file', file, file.name);
+    Axios.post('https://localhost:5001/Home/SingleFile', fd)
+      .then(res => {
+        console.log(res);
+        setText(res.data.text);
+        setMostUsedWord(res.data.mostUsedWord);
+        setFreq(res.data.frequencies);
+        setLoading(false)
+      });
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div class="file-and-upload-container">
-                    <label for="file-upload" id="file-label">Choose file...</label>
-                    <input
-                        id="file-upload"
-                        input type="file"
-                        accept=".rtf,.txt,.md,.file"
-                        onChange={fileSelectedHandler}
-                    />
-                <button type="submit" class="upload-button">Upload</button>
-                </div>
-                <div class="text-container"> 
-                    { text ? (
-                        <pre class="text-box-after">{text}</pre>
-                    ) : (
-                        <p class="text-box-before">Displaying text here...</p>
-                    )}
-                    
-                </div>
-            </form>
-        </div>   
-    );
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div class="input-group mb-3">
+          <div class="custom-file">
+            <input type="file" class="custom-file-input" id="inputGroupFile02" type="file"
+              accept=".rtf,.txt,.md,.file"
+              onChange={fileSelectedHandler} />
+            <label class="custom-file-label" for="inputGroupFile02" aria-describedby="inputGroupFileAddon02">Choose file</label>
+          </div>
+          <div class="input-group-append">
+            <button class="input-group-text" type="submit" id="inputGroupFileAddon02">Upload</button>
+          </div>
+        </div>
+      {text ? (
+        <p className="most-used-word p-1">Your most used word is: { mostUsedWord} <br /> It was used { freq} times!</p>
+      ) : (
+          <div></div>
+        )}
+      <div className="container mt-3 overflow-auto">
+        {text ? (
+          <pre>{text}</pre>
+        ) : (
+            <p className="text-box-before"> {loading ?
+              <div class="spinner-border text-success" role="status">
+                <span class="sr-only">Loading...</span>
+              </div > : 'Displaying text here...'}</p>
+          )}
+
+      </div>
+      </form>
+    </div >
+  );
 }
 
 
